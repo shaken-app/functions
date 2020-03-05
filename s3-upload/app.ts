ed96d09820b0da4b846c86b809c835a9cb2fde52
mattcoady <mattcoady@gmail.com>
2020-03-04 16:10:52 -0800

    Init

---
+++ s3-upload/app.ts
@@ -0,0 +1,39 @@
+import { v4 as uuidv4 } from "uuid";
+import AWS from "aws-sdk";
+
+AWS.config.update({ region: process.env.REGION || "us-east-1" });
+
+const s3 = new AWS.S3({
+  accessKeyId: process.env.aws_id,
+  secretAccessKey: process.env.aws_secret
+});
+
+exports.lambdaHandler = async () => await getUploadURL();
+
+const getUploadURL = async () => {
+  const actionId = uuidv4();
+  const s3Params = {
+    Bucket: "shaken-assets",
+    Key: `${actionId}.jpg`,
+    ACL: "public-read"
+  };
+
+  return new Promise((resolve, reject) => {
+    let uploadURL = s3.getSignedUrl("putObject", s3Params);
+    if (uploadURL) {
+      resolve({
+        statusCode: 200,
+        isBase64Encoded: false,
+        headers: { "Access-Control-Allow-Origin": "*" },
+        body: JSON.stringify({
+          uploadURL,
+          photoFilename: `${actionId}.jpg`
+        })
+      });
+    } else {
+      reject({
+        statusCode: 400
+      });
+    }
+  });
+};
